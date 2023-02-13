83891a2a25beb0b848100ee8f7cf703415dd4ff9
Filip Pazera <pazeraf@gmail.com>
2023-02-13 23:57:00 +0100

    fix authorize

--- src/middleware/authorization.ts
+++ src/middleware/authorization.ts
@@ -7,12 +7,13 @@ export enum roles {
 
 export function authorize(...roles: roles[]) {
 	return async function (req, res, next) {
+		if (roles.length === 0) {
+			return next();
+		}
 		if (req.signedCookies.user) {
 			const user = req.signedCookies.user;
 			const user_role = await db.get_user_role(user);
-			if (roles.length == 0 ||
-				roles.some(role => role === user_role)
-			) {
+			if (roles.some(role => role === user_role)) {
 				req.user = user;
 				return next();
 			}
